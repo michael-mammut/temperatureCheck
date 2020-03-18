2d0bc28a83476217fcc4c12e3b0316ddc1c27be7
MWK <michael.mammut@gmail.com>
2020-03-18 15:39:25 +0100

    Change Sensortype ( you can list the type by  `w1thermsensor ls` on your rpi)

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Check the temperature of my aquarium and send me a push notification
 ### APIs
 * w1thermsensor (https://github.com/timofurrer/w1thermsensor)
 * pushbullet (https://pypi.org/project/pushbullet.py/0.9.1/)
+
+####This is just a little project to fool around and spend time to escape the world. So there is no warranty!!!
--- temperatureCheck.py
+++ temperatureCheck.py
@@ -1,26 +1,26 @@
 from w1thermsensor import W1ThermSensor
 from pushbullet import PushBullet
-from settings import PUSHBULLETTOKEN, TEMPERATURE_LIMITS, WATER_SENSOR_ID
+from settings import PUSHBULLETTOKEN, TEMPERATURE_LIMITS, WATER_SENSOR_ID, AMBIETEN_SENSOR_ID
 
 
-def getTemperatureInCelsius():
-    sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, WATER_SENSOR_ID)
+def getTemperatureInCelsius(id):
+    sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, id)
     return sensor.get_temperature(W1ThermSensor.DEGREES_C)
 
 
-def pushNotification(temperature, title="no title"):
-    message = "Wassertemp. liegt bei: " + str(temperature) + "°C"
+def pushNotification(waterTemperature, ambientTemperature, title="no title"):
+    message = "Wassertemp. liegt bei: " + str(waterTemperature) + "°C. Raumtemperatur bei " + ambientTemperature + "°C"
     pb = PushBullet(PUSHBULLETTOKEN)
     pb.push_note(title, message)
 
-
-temperature = getTemperatureInCelsius()
+ambientTemperature = getTemperatureInCelsius(AMBIETEN_SENSOR_ID)
+temperature = getTemperatureInCelsius(WATER_SENSOR_ID)
 
 if temperature < TEMPERATURE_LIMITS["SETPOINT"]:
-    pushNotification(temperature, "ACHTUNG! GERINGE TEMPERATUR")
+    pushNotification(temperature,ambientTemperature, "ACHTUNG! GERINGE TEMPERATUR")
 elif TEMPERATURE_LIMITS["WARNING"] <= temperature < TEMPERATURE_LIMITS["ALERT"]:
-    pushNotification(temperature, "ACHTUNG! WARME TEMPERATUR")
+    pushNotification(temperature,ambientTemperature, "ACHTUNG! WARME TEMPERATUR")
 elif temperature >= TEMPERATURE_LIMITS["ALERT"]:
-    pushNotification(temperature, "ACHTUNG! ZU WARM")
+    pushNotification(temperature,ambientTemperature, "ACHTUNG! ZU WARM")
 else:
-    pushNotification(temperature, "INFO")
+    pushNotification(temperature,ambientTemperature, "INFO")
