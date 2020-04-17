5833f6cedf6bfc1a3c84fc95b8cb0677f3d7f90a
MWK <michael.mammut@gmail.com>
2020-04-17 16:55:28 +0200

    Change title of notification by given temperature

--- Services/NotificationServiceFactory.py
+++ Services/NotificationServiceFactory.py
@@ -4,16 +4,17 @@ from pushbullet import PushBullet
 from Entities.TemperatureMeasure import MeasureResult
 from Settings.Constants import NOTIFICATIONTYPE
 from Settings.settings import PUSHBULLETTOKEN
+from Settings.temperature_settings import TEMPERATURE_LIMITS
 
 
 class NotificationServiceFactory:
-    def __init__(self, type, measure_result, title='no title'):
+    def __init__(self, type, measure_result, title='Notification title'):
         if type is None or not isinstance(measure_result,
                                           MeasureResult) or measure_result.value is None or measure_result.ambient is None:
             raise TypeError('None is not allowed')
         self._type = type
         self._measureResult = measure_result
-        self._title = title
+        self._title = self._get_notification_title()
         self._message = "Wassertemp. liegt bei: " + str(self._measureResult.value) + "°C. Raumtemperatur bei " + str(
             self._measureResult.ambient) + "°C"
 
@@ -29,9 +30,18 @@ class NotificationServiceFactory:
     def get_title(self):
         return self._title
 
+    def _get_notification_title(self):
+        if self._measureResult.value < TEMPERATURE_LIMITS["SETPOINT"]:
+            return "ACHTUNG! GERINGE TEMPERATUR"
+        elif TEMPERATURE_LIMITS["WARNING"] <= self._measureResult.value < TEMPERATURE_LIMITS["ALERT"]:
+            return "ACHTUNG! WARME TEMPERATUR"
+        elif self._measureResult.value >= TEMPERATURE_LIMITS["ALERT"]:
+            return  "ACHTUNG! ZU WARME TEMPERATUR"
+        else:
+            return "INFORMATION"
+
     def notify(self):
         pass
-
 
 class MobileNotificationService(NotificationServiceFactory):
 
--- test/test_NotificationRepository.py
+++ test/test_NotificationRepository.py
@@ -3,12 +3,15 @@ from unittest import TestCase
 from Entities.TemperatureMeasure import MeasureResult
 from Services.NotificationServiceFactory import MobileNotificationService, NotificationServiceFactory
 from Settings.Constants import NOTIFICATIONTYPE
+from Settings.temperature_settings import TEMPERATURE_LIMITS
 
 
 class TestPushBulletNotification(TestCase):
 
     def setUp(self):
-        self.__measureResult = MeasureResult(10, 30)
+        self.__AMBIENT = 30
+        self.__measureResult = MeasureResult(10, self.__AMBIENT)
+        self.__typeMobile = NOTIFICATIONTYPE.get('MOBILE')
 
     def test_init(self):
         with self.assertRaisesRegex(TypeError, 'None is not allowed') as cm:
@@ -16,15 +19,34 @@ class TestPushBulletNotification(TestCase):
 
     def test_get_message(self):
         nr = MobileNotificationService(self.__measureResult, 'Test Title')
-        self.assertEqual("Test Title", nr.get_title())
         self.assertEqual("Wassertemp. liegt bei: 10°C. Raumtemperatur bei 30°C", nr.get_message())
 
     def test_get_notification_by_type(self):
-        type = NOTIFICATIONTYPE.get('MOBILE')
-        self.assertIsInstance(NotificationServiceFactory(type, self.__measureResult).get_notification_service(),
+
+        self.assertIsInstance(NotificationServiceFactory(self.__typeMobile, self.__measureResult).get_notification_service(),
                               MobileNotificationService)
 
     def test_get_notification_by_type_fail(self):
         type = -1
         with self.assertRaisesRegex(TypeError, 'Notificationtype does not exists'):
             NotificationServiceFactory(type, self.__measureResult).get_notification_service()
+
+    def test_title_by_temperature_information(self):
+        rm = MeasureResult(TEMPERATURE_LIMITS.get('SETPOINT'), self.__AMBIENT)
+        n = NotificationServiceFactory(self.__typeMobile, rm).get_notification_service()
+        self.assertEqual('INFORMATION', n.get_title())
+
+    def test_title_by_temperature_warning_cold(self):
+        rm = MeasureResult(10, self.__AMBIENT)
+        n = NotificationServiceFactory(self.__typeMobile, rm).get_notification_service()
+        self.assertEqual('ACHTUNG! GERINGE TEMPERATUR', n.get_title())
+
+    def test_title_by_temperature_warning_warm(self):
+        rm = MeasureResult(TEMPERATURE_LIMITS.get('WARNING'), self.__AMBIENT)
+        n = NotificationServiceFactory(self.__typeMobile, rm).get_notification_service()
+        self.assertEqual('ACHTUNG! WARME TEMPERATUR', n.get_title())
+
+    def test_title_by_temperature_alert_warm(self):
+        rm = MeasureResult(TEMPERATURE_LIMITS.get('ALERT'), self.__AMBIENT)
+        n = NotificationServiceFactory(self.__typeMobile, rm).get_notification_service()
+        self.assertEqual('ACHTUNG! ZU WARME TEMPERATUR', n.get_title())
