9276827ffb941707e7975dad84d702edf2919ffc
MWK <michael.mammut@gmail.com>
2020-04-21 08:04:33 +0200

    Add new business execution code

--- temperatureCheck.py
+++ temperatureCheck.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+from Entities.TemperatureMeasure import TemperatureMeasure
+from Services.NotificationServiceFactory import NotificationServiceFactory
+from Services.TemperatureService import TemperatureService
+from Settings.constants import NOTIFICATIONTYPE
+from Settings.temperature_settings import WATER_SENSOR_ID, AMBIETEN_SENSOR_ID
 
-#toDo implement script
+temperatureService = TemperatureService(TemperatureMeasure(measurement_name='Aquarium', sensor_id=WATER_SENSOR_ID,
+                                                           sensor_label='Wassertemperatur',
+                                                           reference_sensor_id=AMBIETEN_SENSOR_ID))
+measurer_result = temperatureService.get_temperature_in_celsius()
+
+notification_service = NotificationServiceFactory(NOTIFICATIONTYPE.get('MOBILE'), measurer_result)
+mobile = notification_service.get_notification_service()
+mobile.notify()
