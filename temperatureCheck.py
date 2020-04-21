4662d365504118faabd9e629e7053f63d01441c1
MWK <michael.mammut@gmail.com>
2020-04-21 20:32:24 +0200

    Write measure data to CSV file

--- temperatureCheck.py
+++ temperatureCheck.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-from Entities.TemperatureMeasure import TemperatureMeasure
+
+import Entities.TemperatureMeasure
+from Repository.DataRepository import DataRepository
 from Services.NotificationServiceFactory import NotificationServiceFactory
 from Services.TemperatureService import TemperatureService
 from Settings.constants import NOTIFICATIONTYPE
 from Settings.temperature_settings import WATER_SENSOR_ID, AMBIETEN_SENSOR_ID
+from Settings.datastore import TEMPERATURE_DATA_FILE
 
-temperatureService = TemperatureService(TemperatureMeasure(measurement_name='Aquarium', sensor_id=WATER_SENSOR_ID,
-                                                           sensor_label='Wassertemperatur',
-                                                           reference_sensor_id=AMBIETEN_SENSOR_ID))
+temperatureService = TemperatureService(Entities.TemperatureMeasure.TemperatureMeasure(measurement_name='Aquarium', sensor_id=WATER_SENSOR_ID,
+                                                                                       sensor_label='Wassertemperatur',                                                                              reference_sensor_id=AMBIETEN_SENSOR_ID))
 measurer_result = temperatureService.get_temperature_in_celsius()
+
+data_repo = DataRepository(TEMPERATURE_DATA_FILE)
+data_repo.add(measurer_result)
 
 notification_service = NotificationServiceFactory(NOTIFICATIONTYPE.get('MOBILE'), measurer_result)
 mobile = notification_service.get_notification_service()
