fd8b47f487cc72da34ebe8274527457356f6dc2a
MWK <michael.mammut@gmail.com>
2020-05-14 10:34:49 +0200

    Add columns to write data call ( should be refactored )

--- temperatureCheck.py
+++ temperatureCheck.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import Entities.TemperatureSensor
 from Repository.CsvDataRepository import CsvDataRepository
 from Services.NotificationServiceFactory import NotificationServiceFactory
 from Settings.constants import NOTIFICATIONTYPE
@@ -11,7 +10,7 @@ from Entities.TemperatureSensor import TemperatureSensor
 temperatureSensor = TemperatureSensor('Aquarium', WATER_SENSOR_ID, 'Wassertemperatur', AMBIETEN_SENSOR_ID)
 measurer_result = temperatureSensor.get_temperature_in_celsius()
 
-data_repo = CsvDataRepository(TEMPERATURE_DATA_FILE)
+data_repo = CsvDataRepository(TEMPERATURE_DATA_FILE, ['value', 'ambient', 'created_at'])
 data_repo.add(measurer_result)
 
 notification_service = NotificationServiceFactory(NOTIFICATIONTYPE.get('MOBILE'), measurer_result)
